c5efaceb044b7074e09e7d19eb126ebd3d94b479
Chang Yeyun <yeyun.chang05@gmail.com>
2025-09-24 22:34:33 -0400

    retrievemethod

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -88,10 +88,17 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+        NumberTriangle current = this;
+        for (char ch: path.toCharArray()){
+            if (ch == '1') {
+                current = current.left;
+            } else if (ch == 'r'){
+                current = current.right;
+            }
+        }
+        return current.root;
     }
-
+retr
     /** Read in the NumberTriangle structure from a file.
      *
      * You may assume that it is a valid format with a height of at least 1,
